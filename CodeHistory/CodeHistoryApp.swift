f57da69689a0b2979a10ac178a4d6a3be09b2570
suryaharahap <suryaharahap18@gmail.com>
2021-06-30 23:30:18 +0700

    initial code

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Surya on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}

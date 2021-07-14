d7affffc9b081dc428c116f0d98c1fc9e89e93ee
suryaharahap <suryaharahap18@gmail.com>
2021-07-14 17:09:28 +0700

    Building the Game Model

---
+++ CodeHistory/Models/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  CodeHistory
+//
+//  Created by Surya on 14/07/21.
+//
+
+import Foundation
+
+struct Question: Hashable {
+  let questionText: String
+  let possibleAnswers: [String]
+  let correctAnswerIndex: Int
+  
+  static var allQuestion = [
+    Question(questionText: "Who invented the World Wide Web?",
+             possibleAnswers: [
+              "Steve Jobs",
+              "Linus Torvalds",
+              "Bill Gates",
+              "Tim Berners-Lee"
+             ],
+             correctAnswerIndex: 3),
+    Question(questionText: "What was the first object oriented programming language?",
+             possibleAnswers: [
+              "Simula",
+              "Python",
+              "Swift",
+              "C",
+             ],
+             correctAnswerIndex: 0
+    )
+  ]
+  
+}
